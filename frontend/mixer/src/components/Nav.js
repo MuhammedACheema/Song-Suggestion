d94c750d1f83f26e4e104adac1e272c73f0da4aa
Muhammed Cheema <cheemam0807@gmail.com>
2025-03-11 17:24:08 -0400

    set up a nav bar, still need to route it, and get it to run properly

---
+++ frontend/mixer/src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Nav.css'
+function Nav(){
+    return(
+        <nav> 
+             <ul>
+                <li><a href = " ">Home</a></li>
+                <li><a href = " ">Recommendations</a></li>
+                <li><a href = " ">Mixer</a></li>
+                <li style={{float:"right"}}><a href = " ">Sign In</a></li>
+             </ul>
+        </nav>
+       
+    )
+}
+
+export default Nav;
